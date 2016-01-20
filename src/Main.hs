3ea61bf2b2d6578d3a131d5248bbbd0d501f6efb
Jacob Errington <git@mail.jerrington.me>
2016-01-20 00:56:59 -0500

    improve main program
    The main program now reads from a file instead of standard in and it
    writes out some diagnostic files containing the pretty-print of the
    parsed brainfuck code as well as the pretty-print of the compiled
    assembly.

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 
 module Main where
 
@@ -9,26 +10,38 @@ import Assembler
 import Compiler
 import Memory
 import Parser
+import Pretty ( pretty_ )
+import PrettyAsm ( pretty )
 
 import Data.Attoparsec.ByteString ( parseOnly )
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.ByteString.Char8 as C8
+import qualified Data.Text.Lazy.IO as TIO
 import Foreign
+import System.Environment ( getArgs )
 import System.Exit ( exitFailure )
 
 main :: IO ()
 main = do
-    stdin <- C8.getContents
-    bf <- case parseOnly brainfuck stdin of
+    arg1:_ <- getArgs
+    file <- C8.readFile arg1
+
+    bf <- case parseOnly brainfuck file of
         Left e -> do
             putStrLn "Parse error: "
             putStrLn e
             exitFailure
         Right x -> return x
 
+    let prettyBf = pretty_ bf
+
+    writeFile "out.bf" prettyBf
+
     let asm = asmFunction $ do
             mov rax rdi
             compileFuck bf
+
+    TIO.writeFile "out.asm" (pretty asm)
 
     code <- case assemble asm of
         Left e -> do
@@ -43,6 +56,7 @@ main = do
             return bs
 
     let b = BS.toStrict code
+    putStrLn $ "Assembled " ++ show (C8.length b) ++ " bytes."
     f <- byteStringFunction b
     mem <- callocBytes 4096
     () <- f mem
